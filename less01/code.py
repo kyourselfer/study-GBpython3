4ae987ec209bd9255b4460761411d64d660c87c5
Vladimir <kyourselfer@gmail.com>
2022-02-26 21:56:57 +0300

    1st lesson. Try to remind what was in the past.

---
+++ less01/code.py
@@ -0,0 +1,11 @@
+age = 30
+name = 'Vladimir'
+money = 150.1
+
+print('Hi', name)
+
+print(age / 2)
+print(age // 2)
+print(age % 2) # Деление по модулю, выполняем для определения остатка или для определение кратное ли число 2
+print(age ** 2)
+
